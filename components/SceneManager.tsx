bf9002db3fe1eb4ff540212bf65595cde733fcec
enriquecortes-io <enriquecortesgomez@gmail.com>
2026-04-26 09:06:42 +0000

    .m.m

--- components/SceneManager.tsx
+++ components/SceneManager.tsx
@@ -1,38 +1,35 @@
 'use client';
 import React, { useRef, Suspense } from 'react';
+import { useThree } from '@react-three/fiber';
 import * as THREE from 'three';
 
 import ServiceCylinder from './ServiceCylinder';
 import ServiceWheelContent from './ServiceWheelContent';
 
 export default function SceneManager() {
+  const { viewport } = useThree();
   const wheelDataRef = useRef({ rotation: 0, activeIndex: 0 });
 
   return (
     <Suspense fallback={null}>
-        {/* 1. LUZ AMBIENTAL MUY FUERTE (Para ver todo sin sombras) */}
-        <ambientLight intensity={2} />
-        <pointLight position={[0, 0, 10]} intensity={2} />
+      {/* 1. LUZ TOTAL: Si esto no los ilumina, nada lo hará */}
+      <ambientLight intensity={2} />
+      <pointLight position={[0, 0, 15]} intensity={3} />
 
-        <group position={[0, 0, 0]}>
-          
-          {/* 🎡 LA RUEDA: Forzamos que ignore su posición interna y esté en el centro */}
-          <group position={[0, 1, 0]} scale={0.8}>
-            <ServiceWheelContent wheelDataRef={wheelDataRef} />
-          </group>
-
-          {/* 🧪 EL CILINDRO: Justo debajo, pegado a la rueda */}
-          <group position={[0, -5, 0]} scale={0.8}>
-            <ServiceCylinder wheelDataRef={wheelDataRef} />
-          </group>
-
-          {/* 🔍 OBJETO DE PRUEBA: Si no ves esto, el problema es la cámara */}
-          <mesh position={[0, 0, -5]}>
-            <sphereGeometry args={[0.2]} />
-            <meshBasicMaterial color="yellow" />
-          </mesh>
-
+      {/* 2. POSICIONAMIENTO EN EL CENTRO (Y=0) */}
+      <group position={[0, 0, 0]}>
+        
+        {/* LA RUEDA: Un poco arriba del centro */}
+        <group position={[0, 3, 0]} scale={0.7}>
+          <ServiceWheelContent wheelDataRef={wheelDataRef} />
         </group>
+
+        {/* EL CILINDRO: Un poco abajo del centro */}
+        <group position={[0, -4, 0]} scale={0.7}>
+          <ServiceCylinder wheelDataRef={wheelDataRef} />
+        </group>
+
+      </group>
     </Suspense>
   );
 }
