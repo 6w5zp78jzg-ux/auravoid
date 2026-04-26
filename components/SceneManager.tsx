d2d7a484fefe4022831adb1d5012f7bfaa0dc1b6
enriquecortes-io <enriquecortesgomez@gmail.com>
2026-04-26 11:09:02 +0000

    m m

--- components/SceneManager.tsx
+++ components/SceneManager.tsx
@@ -1,8 +1,5 @@
 'use client';
 import React, { useRef, Suspense } from 'react';
-import { useThree } from '@react-three/fiber';
-import * as THREE from 'three';
-
 import ServiceCylinder from './ServiceCylinder';
 import ServiceWheelContent from './ServiceWheelContent';
 
@@ -10,28 +7,21 @@ export default function SceneManager() {
   const wheelDataRef = useRef({ rotation: 0, activeIndex: 0 });
 
   return (
-    <Suspense fallback={null}>
-      <ambientLight intensity={1.2} />
-      <pointLight position={[0, 5, 20]} intensity={2.5} color="#00ffff" />
-      <pointLight position={[0, -5, 20]} intensity={1.5} color="#ffffff" />
+    <group>
+      <ambientLight intensity={2} />
+      <pointLight position={[10, 10, 10]} intensity={2} />
 
       <group position={[0, 0, 0]}>
-        {/* 🎡 RUEDA SUPERIOR (Selector) - Más estilizada */}
-        <group position={[0, 5.5, 0]} scale={0.7}>
+        {/* RUEDA ARRIBA */}
+        <group position={[0, 4, 0]}>
           <ServiceWheelContent wheelDataRef={wheelDataRef} />
         </group>
 
-        {/* 🧪 CILINDRO INFERIOR (Información) - Bajado para dar aire */}
-        <group position={[0, -6, 0]} scale={0.7}>
+        {/* CILINDRO ABAJO */}
+        <group position={[0, -5, 0]}>
           <ServiceCylinder wheelDataRef={wheelDataRef} />
         </group>
-
-        {/* EJE DE CONEXIÓN ULTRA FINO */}
-        <mesh position={[0, 0, -2]}>
-          <cylinderGeometry args={[0.01, 0.01, 35, 8]} />
-          <meshBasicMaterial color="#00ffff" transparent opacity={0.15} />
-        </mesh>
       </group>
-    </Suspense>
+    </group>
   );
 }
