95be9c8738f536671f6aa011a86b6f8624d7a83d
enriquecortes-io <enriquecortesgomez@gmail.com>
2026-04-26 09:04:04 +0000

    ,m

--- components/SceneManager.tsx
+++ components/SceneManager.tsx
@@ -1,37 +1,38 @@
 'use client';
-import React, { useRef } from 'react';
-import { useFrame, useThree } from '@react-three/fiber';
-import { useScroll } from '@react-three/drei';
+import React, { useRef, Suspense } from 'react';
 import * as THREE from 'three';
 
 import ServiceCylinder from './ServiceCylinder';
 import ServiceWheelContent from './ServiceWheelContent';
 
 export default function SceneManager() {
-  const { viewport } = useThree();
   const wheelDataRef = useRef({ rotation: 0, activeIndex: 0 });
 
   return (
-    <group position={[0, -viewport.height * 1.5, 0]}>
-      <ambientLight intensity={0.5} />
-      <pointLight position={[10, 10, 10]} intensity={2} />
+    <Suspense fallback={null}>
+        {/* 1. LUZ AMBIENTAL MUY FUERTE (Para ver todo sin sombras) */}
+        <ambientLight intensity={2} />
+        <pointLight position={[0, 0, 10]} intensity={2} />
 
-      {/* 🎡 LA RUEDA (MAESTRO - NIVEL SUPERIOR) */}
-      <group position={[0, 5, 0]}> 
-        <ServiceWheelContent wheelDataRef={wheelDataRef} />
-      </group>
+        <group position={[0, 0, 0]}>
+          
+          {/* 🎡 LA RUEDA: Forzamos que ignore su posición interna y esté en el centro */}
+          <group position={[0, 1, 0]} scale={0.8}>
+            <ServiceWheelContent wheelDataRef={wheelDataRef} />
+          </group>
 
-      {/* 🧪 EL CILINDRO (DETALLE - NIVEL INFERIOR) */}
-      {/* Lo bajamos en Y para que parezca una extensión de la rueda */}
-      <group position={[0, -5, 0]}> 
-        <ServiceCylinder wheelDataRef={wheelDataRef} />
-      </group>
+          {/* 🧪 EL CILINDRO: Justo debajo, pegado a la rueda */}
+          <group position={[0, -5, 0]} scale={0.8}>
+            <ServiceCylinder wheelDataRef={wheelDataRef} />
+          </group>
 
-      {/* EFECTO DE CONEXIÓN: Un haz de luz central que une ambos */}
-      <mesh position={[0, 0, 0]}>
-        <cylinderGeometry args={[0.05, 0.05, 20, 8]} />
-        <meshBasicMaterial color="#00ffff" transparent opacity={0.2} />
-      </mesh>
-    </group>
+          {/* 🔍 OBJETO DE PRUEBA: Si no ves esto, el problema es la cámara */}
+          <mesh position={[0, 0, -5]}>
+            <sphereGeometry args={[0.2]} />
+            <meshBasicMaterial color="yellow" />
+          </mesh>
+
+        </group>
+    </Suspense>
   );
 }
